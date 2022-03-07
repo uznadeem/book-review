48e26d11c214ea31ddc3c71031493cfebc3df2df
uznadeem <uzair_nadeem@live.com>
2022-03-07 01:58:36 -0800

    Reviews: Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  concern :reviewable do
+    resources :reviews, only: [:create, :index]
+  end
+
   namespace :api do
-    resources :authors, only: [:create, :index, :show, :update]
-    resources :books, only: [:create, :index, :show, :update]
+    resources :authors, only: [:create, :index, :show, :update], concerns: :reviewable
+    resources :books, only: [:create, :index, :show, :update], concerns: :reviewable
     resources :users, only: [:create, :index, :show, :update]
   end
 end
